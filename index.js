5d2c34391b3e287a01702b58193b12b992352313
Pavlo Lytvyshko <pavlo.lytvyshko@jointoit.com>
2026-01-29 01:01:33 +0200

    add styles for a note row

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-})
+const posts = [];
 
+const renderIndex = (req, res) => {
+  const { status } = req.query;
+  console.log(status);
+
+  res.render("index.ejs");
+};
+
+app.get("/", renderIndex);
+app.get("/posts", renderIndex);
+
+app.post("/submit", (req, res) => {
+  if (req.body.noteHeader) {
+    posts.push({
+      id: posts.length + 1,
+      title: req.body.noteHeader,
+      text: req.body.noteDescription || '',
+      isComplete: false,
+    });
+  }
+
+  res.render("index.ejs", { posts });
+});
 
 app.listen(port, () => {
   console.log(`App is running on port: ${port}`);
-})
+});
